c244fbbb62189df86f5e20fe86b6710d25aa1b95
oltoir <68994372+oltoir@users.noreply.github.com>
2022-12-24 17:47:59 +0600

    feat: heroku deploy settings

--- src/templates/AppTemplate/index.jsx
+++ src/templates/AppTemplate/index.jsx
@@ -1,13 +1,13 @@
-import {Box} from "@chakra-ui/react";
-import {Header} from "components/Header/index";
+import { Box } from "@chakra-ui/react";
+import { Header } from "components/Header/index";
 
-export default function AppTemplate({children}) {
-    return(
-        <>
-            <Header/>
-            <Box backgroundColor='gray.100' h='100vh' pt='60px' >
-            {children}
-            </Box>
-        </>
-    )
+export default function AppTemplate({ children }) {
+  return (
+    <>
+      <Header />
+      <Box backgroundColor="gray.100" minH="100vh" h="100%" pt="60px">
+        {children}
+      </Box>
+    </>
+  );
 }
