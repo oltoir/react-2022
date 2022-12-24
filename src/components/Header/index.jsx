fc3abef61bd6ac37d044463e7ce69236dfe2be1c
altair <altair@1fit.app>
2022-12-24 17:18:57 +0600

    chore: some small changes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,27 +1,27 @@
-import {Box, Flex, Link, Text} from "@chakra-ui/react";
+import { Box, Flex, Link, Text } from "@chakra-ui/react";
 
-export function Header(){
-    return(
-        <Flex
-            gap='32px'
-            h='60px'
-            alignItems='center'
-            w='100%'
-            backgroundColor='rgba( 88, 129, 255, 0.25 );'
-            boxShadow='0 8px 32px 0 rgba( 31, 38, 135, 0.37 );'
-            backdropFilter='blur( 4px );'
-            position='fixed'
-            px='200px'
-        >
-            <Link href='/news'>
-                <Text fontWeight='semibold'>Новости</Text>
-            </Link>
-            <Link href='/schedule'>
-                <Text fontWeight='semibold'>Расписание</Text>
-            </Link>
-            <Link href='/account'>
-                <Text fontWeight='semibold'>Мой аккаунт</Text>
-            </Link>
-        </Flex>
-    )
+export function Header() {
+  return (
+    <Flex
+      gap="32px"
+      h="60px"
+      alignItems="center"
+      w="100%"
+      backgroundColor="rgba( 88, 129, 255, 0.25 );"
+      boxShadow="0 8px 32px 0 rgba( 31, 38, 135, 0.37 );"
+      backdropFilter="blur( 4px );"
+      position="fixed"
+      px="200px"
+    >
+      <Link href="/">
+        <Text fontWeight="semibold">Новости</Text>
+      </Link>
+      <Link href="/schedule">
+        <Text fontWeight="semibold">Расписание</Text>
+      </Link>
+      <Link href="/account">
+        <Text fontWeight="semibold">Мой аккаунт</Text>
+      </Link>
+    </Flex>
+  );
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,51 +1,46 @@
-import {lazy, Suspense} from "react";
-import {
-    BrowserRouter as Router,
-    useRoutes,
-} from 'react-router-dom';
-import LoadingTemplate from 'templates/LoadingTemplate';
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, useRoutes } from "react-router-dom";
+import LoadingTemplate from "templates/LoadingTemplate";
 import NewsDetailPage from "../pages/NewsDetail";
 
-const NewsPage = lazy(()=>import('pages/News'))
-const TestPage = lazy(()=>import('pages/Test'))
-const SchedulePage = lazy(()=>import('pages/Schedule'))
-const AccountPage = lazy(()=>import('pages/Account'))
+const NewsPage = lazy(() => import("pages/News"));
+const TestPage = lazy(() => import("pages/Test"));
+const SchedulePage = lazy(() => import("pages/Schedule"));
+const AccountPage = lazy(() => import("pages/Account"));
 
 function RootRouterWrap() {
+  const routes = [
+    {
+      path: "/",
+      element: <NewsPage />,
+    },
+    {
+      path: "/news/:id",
+      element: <NewsDetailPage />,
+    },
+    {
+      path: "/test",
+      element: <TestPage />,
+    },
+    {
+      path: "/schedule",
+      element: <SchedulePage />,
+    },
+    {
+      path: "/account",
+      element: <AccountPage />,
+    },
+  ];
 
-    const routes = [
-        {
-            path: '/news',
-            element: <NewsPage/>
-        },
-        {
-            path: '/news/:id',
-            element: <NewsDetailPage/>
-        },
-        {
-            path: '/test',
-            element: <TestPage/>
-        },
-        {
-            path: '/schedule',
-            element: <SchedulePage/>
-        },
-        {
-            path: '/account',
-            element: <AccountPage/>
-        }
-    ]
-
-    return <>{useRoutes(routes)}</>;
-
+  return <>{useRoutes(routes)}</>;
 }
 
 export function RootRouter() {
-    return (
-        <Suspense fallback={<LoadingTemplate />}>
-            <Router>
-                <RootRouterWrap/>
-            </Router>
-        </Suspense>
-    );
+  return (
+    <Suspense fallback={<LoadingTemplate />}>
+      <Router>
+        <RootRouterWrap />
+      </Router>
+    </Suspense>
+  );
 }
